a1010b90ef988fbb014beeb42d92d25dca2f844b
AliGurel <aliigurel@gmail.com>
2024-04-15 22:37:17 +0300

    pre

--- src/models/user.js
+++ src/models/user.js
@@ -66,9 +66,9 @@ const UserSchema = new mongoose.Schema({
         unique: [true, 'Email field must be unique'],
         index: true,
         validate: [
-            (email) => { 
-                const regexEmailCheck=/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
-                return regexEmailCheck.test(email)                
+            (email) => {
+                const regexEmailCheck = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+                return regexEmailCheck.test(email)
             },
             'Email type is not correct.'
         ]
@@ -92,14 +92,29 @@ const UserSchema = new mongoose.Schema({
         default: false,
     },
 
-},{
-    collection: 'users', 
+}, {
+    collection: 'users',
     timestamps: true
 })
 //https://mongoosejs.com/docs/middleware.html#pre
 //pre middleware ler
-//hocanın tavsiyesi set ve validasyon işlemini yukarda field da yapmamız mış, pre yi göstermek için burda yapmış
+//hocanın tavsiyesi set ve validasyon işlemini yukarda field da yapmamız mış, pre yi göstermek için burda yapmış, kullanıcağımız ve kullanmayı tavsiye ettiği bi yöntem değilmiş
+UserSchema.pre('save', function (next) {
+    //data this parametressinin içinde
+    // console.log(this)
+    const data = this
+    //email@domain.com
+    // email varsa kontrol et yoksa direkt true de
+    const isEmailValidated = data.email ? /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(data.email) : true
+
+    if (isEmailValidated) {
+        console.log('Email OK')
+        next()
+    } else {
+        next(new Error('Email is not validated')) // middleware içinden errorHandler a hata gönderme bu şeklde olur
+    }
+    
+})
 
 
-
-module.exports = mongoose.model("User",UserSchema)
+module.exports = mongoose.model("User", UserSchema)
