aba0287298fbcaf0a1b0c679e1e82b9f05c3d54e
AliGurel <aliigurel@gmail.com>
2024-04-15 23:16:05 +0300

    up

--- src/models/user.js
+++ src/models/user.js
@@ -38,6 +38,27 @@ const passwordEncrypt = require('../helpers/passwordEncrypt')
     "isAdmin": false
 }
 /* ------------------------------------------------------- */
+
+/* REGEX AÇIKLAMASI
+
+/ - Regex başlangıç sınırlayıcısı
+
+^ - String'in başlangıcını belirtir
+
+\w+ - Bir veya daha fazla alfabetik karakter, rakam veya underscore (_) karakteri. Bunlar e-posta adresinin kullanıcı adı bölümünü temsil eder.
+
+([\.-]?\w+)* - Nokta (.) veya tire (-) ve ardından bir veya daha fazla alfanumerik karakterden oluşan opsiyonel bir grup. E-posta adresinde bölüm ayırıcıları olarak kullanılabilir. * işareti bu grubun sıfır veya daha fazla kez tekrar edebileceğini gösterir.
+
+\@ - Bir @ sembolü arar.
+
+\w+ - E-posta adresinin domain kısmındaki alfanumerik karakterler.
+
+(\.[a-z]{2,3})+ - Nokta (.) ve ardından 2 veya 3 harften oluşan bir grup. Bu, e-posta adresinin uzantısını (com, net vb.) temsil eder.
+
+$ - String'in bitişini belirtir.
+
+*/
+
 const UserSchema = new mongoose.Schema({
     username: {
         type: String,
@@ -65,6 +86,8 @@ const UserSchema = new mongoose.Schema({
         required: [true, 'Email field must be required'],
         unique: [true, 'Email field must be unique'],
         index: true,
+        //aşağıdaki validasyon işlemini ister burda ister aşağıdaki pre(save) middlewarinde de yapabiliriz
+        //ama doğrusu burada yapmak dedi hoca
         validate: [
             (email) => {
                 const regexEmailCheck = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
@@ -96,25 +119,58 @@ const UserSchema = new mongoose.Schema({
     collection: 'users',
     timestamps: true
 })
+//? PRE MIDDLEWARE
+/*
 //https://mongoosejs.com/docs/middleware.html#pre
-//pre middleware ler
+//pre(save) middleware ler create de çalışır ama updateOne de çalışmazmış
+//bunun için ['save,'updateOne'] kullanıcaz
 //hocanın tavsiyesi set ve validasyon işlemini yukarda field da yapmamız mış, pre yi göstermek için burda yapmış, kullanıcağımız ve kullanmayı tavsiye ettiği bi yöntem değilmiş
-UserSchema.pre('save', function (next) {
-    //data this parametressinin içinde
+// UserSchema.pre('save', function (next) {
+UserSchema.pre(['save','updateOne'], function (next) {
+    //gelen data this parametresinin içinde
     // console.log(this)
-    const data = this
+    //şimdilik this içindeki verileri data değişkenine atayıp kontrolleri bu değişken üzerinde yapalım
+    //işimiz bitince this de güncelleyelim
+
+    // updateOne yaptığımızda, data blgisi _update isimli değişkende geliyor, direkt this de gelmiyor, create yaptığımızda direkt olarak this de geliyordu, o nedenle aşağıdaki kontrolü yaptı
+    const data = this?._update || this
     //email@domain.com
     // email varsa kontrol et yoksa direkt true de
     const isEmailValidated = data.email ? /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(data.email) : true
 
     if (isEmailValidated) {
         console.log('Email OK')
+        if(data?.password) { //data içinde password varsa kontrol et, gönderilmemişse kaale alma
+            const isPasswordValidated = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&]).{8,}$/.test(data.password)
+
+            if (isPasswordValidated) {
+                console.log('Password OK')
+
+                data.password = passwordEncrypt(data.password)
+                if (this?._update) {
+                    this._update = data
+                    // this._update.password = data.password 
+
+                } else {
+                    // this = data // izin vermiyor.
+                    this.password = data.password
+                }
+                //? ShortHand:
+                // // save:
+                // this.password = data.password = passwordEncrypt(data.password)
+                // // update:
+                // this._update = data
+
+            }else {
+                next(new Error('Password is not validated')) // middleware içinden errorHandler a hata gönderme bu şeklde olur
+            }
+        }
         next()
     } else {
         next(new Error('Email is not validated')) // middleware içinden errorHandler a hata gönderme bu şeklde olur
     }
     
 })
-
+*/
 
 module.exports = mongoose.model("User", UserSchema)
